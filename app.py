0810e6ace372d30471bb33f17a5a766971e5dbe1
rizkisadewa <rizkykysadewa@gmail.com>
2018-09-19 07:57:41 +0800

    workin on register

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging #stuff from Flask
 from data import Articles #import the data from data.py
+from flaskext.mysql import MySQL # import from MYSQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 #use the function of Article() so that we can return the value of the data
 Articles = Articles()
+
+mysql = MySQL()
 
 # Create a route
 @app.route('/')
@@ -21,6 +26,25 @@ def articles():
 @app.route('/article/<string:id>/')
 def article(id):
     return render_template('article.html', id=id)
+
+# Form Validator for Register
+class RegisterFrom(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username',[validators.Length(min=4, max=25)])
+    email = StringField('Email',[validators.Length(min=6, max=50)])
+    password = StringField('Password',[
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Password do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+#route for register
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterFrom(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html')
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
